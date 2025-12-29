3ef2270c795a9b670631366b55138a9ced1151a3
VladmiraTheDev <vlamira1402@gmail.com>
2025-12-29 12:48:00 +0300

    fix: leaflet import and install dependencies

--- web/src/components/PropertyMap/PropertyMap.tsx
+++ web/src/components/PropertyMap/PropertyMap.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import L from 'leaflet'
+import * as L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import styles from './PropertyMap.module.scss'
 import type { Property } from '../../data/mockProperties'
